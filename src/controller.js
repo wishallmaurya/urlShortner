0d27f860923d30e93f1f42bbe1b2b67188f71912
wishallmaurya <vm7740014@gmail.com>
2022-07-18 17:22:30 +0530

    redirect done

--- src/controller.js
+++ src/controller.js
@@ -3,25 +3,27 @@ const validUrl = require('valid-url');
 const shortId = require('shortid');
 
 
-const isValid = function (value) {
-    if (typeof value === "undefined" || value === null) return false;
-    if (typeof value === "string" && value.trim().length == 0) return false;
-    return true;
-}
+// const isValid = function (value) {
+//     if (typeof value === "undefined" || value === null) return false;
+//     if (typeof value === "string" && value.trim().length == 0) return false;
+//     return true;
+// }
 
 exports.createurl = async function (req, res) {
     try {
         let data = req.body;
         
-        if (!isValid(data.longUrl)) {
-            return res.status(400).send({ status: false, message: "write your longUrl its mandatory" })
-        }
+        // if (!isValid(data.longUrl)) {
+        //     return res.status(400).send({ status: false, message: "write your longUrl its mandatory" })
+        // }
         
         if (!validUrl.isUri(data.longUrl)){
             return res.status(400).send({ status: false, message: "longUrl is invalid" })} 
 
         
         checkUniqueUrl = await urlModel.findOne({ longUrl: data.longUrl })
+        
+    
         if (checkUniqueUrl) {
             return res.status(400).send({ status: false, message: "link is already shorted", data: checkUniqueUrl })
         }
@@ -46,17 +48,12 @@ exports.redirectUrl = async function (req, res){
     try {
         const urlCode = req.params.urlCode;
        
-        const chkUrlCode = await urlModel.findOne({
-            urlCode: urlCode,
-        })
+        const chkUrlCode = await urlModel.findOne({urlCode: urlCode})
         if (chkUrlCode) {
-            return res.status(301).send({data:chkUrlCode})
+            return res.redirect(302,chkUrlCode.longUrl)
         }
         else{
-            return res.status(404).send({
-                status: false,
-                message: "Url Not Found!",
-            })
+            return res.status(404).send({status: false,message: "Url Not Found!"})
         }
     }  
     catch (error) {
