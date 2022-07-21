0382a42c28100c504917aeada4105e11ed8e9af3
wishallmaurya <vm7740014@gmail.com>
2022-07-21 19:13:24 +0530

    proper order

--- src/model.js
+++ src/model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const urlSchema = new mongoose.Schema(
   {
-    urlCode: { type: String, required: true,unique: true ,lowercase: true, trim:true },
     longUrl: {type: String,required: true, unique: true },
     shortUrl: { type: String,required: true,unique:true},
+    urlCode: { type: String, required: true,unique: true ,lowercase: true, trim:true },
   }
 );
 module.exports = mongoose.model("url", urlSchema);
